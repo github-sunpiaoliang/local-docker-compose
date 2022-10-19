7f14e81b9d44edd7be1d42ff98dc298b09163881
sunpeng <sunpeng@sunpiaoliang.cn>
2022-10-19 13:24:01 +0800

    调整nginx

--- volumes/nginx/init.sh
+++ volumes/nginx/init.sh
@@ -1,10 +1,5 @@
 #! /bin/bash
 
-mkdir conf html logs
-
-docker run --name nginx -d nginx:1.23
-docker cp nginx:/etc/nginx/. ./conf
-docker cp nginx:/usr/share/nginx/html/. ./html
-docker rm -f nginx
+mkdir html logs
 
 
